ae9d41ddebe409262c315b439ce6dcdfb5e12541
Leo Zhang <leotheasianlion@gmail.com>
2022-01-26 23:56:25 -0500

    update

---
+++ ScrapeCraigslist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+from bs4 import BeautifulSoup as soup
+
+
+
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36", 'Accept-Language': 'en-US, en;q=0.5'}
+
+page = requests.get("https://boston.craigslist.org/d/automotive-services/search/aos", headers=HEADERS)
+print(page.status_code)
+
+
+
+bsobj = soup(page.content,'html.parser')
+# bsobj
+
+
+
+links = []
+for link in bsobj.findAll('li',{'class':'result-row'}):
+    links.append(link.a['href'])
+
+# links
+
+
+
+title = []
+
+# for link in links:
+#     page = requests.get(link, headers=HEADERS)
+#     bsobj = soup(page.content,'html.parser')
+#     print(bsobj.findAll('h1')[0].text.strip())
+#     title.append(bsobj.findAll('h1')[0].text.strip())
+
+#     for i in bsobj.findAll('section',{'id':'postingbody'}):
+#         print(i.text.strip())
+
+
+
+# len(title)
+
